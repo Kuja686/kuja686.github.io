c567e5305d1c07a9f1819b5f8e222e6f63009445
Kevin Schmitt <schmittkevin@gmx.de>
2024-05-17 14:20:50 +0200

    add everything to repository

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const inputSolution1 = document.getElementById('solution1')
+
+inputSolution1.onkeyup = function(){
+    document.getElementById('printSolution1').innerHTML = inputSolution1.value;
+}
+
+const inputSolution2 = document.getElementById('solution2')
+
+inputSolution2.onkeyup = function(){
+    document.getElementById('printSolution2').innerHTML = inputSolution2.value;
+}
+
+const inputSolution3 = document.getElementById('solution3')
+
+inputSolution3.onkeyup = function(){
+    document.getElementById('printSolution3').innerHTML = inputSolution3.value;
+}
+
+function check(){
+    part1 = document.getElementById('solution1').value
+    part2 = document.getElementById('solution2').value
+    part3 = document.getElementById('solution3').value
+
+    if (part1.toLowerCase().trim() == '10' && part2.toLowerCase().trim() == 'welt' && part3.toLowerCase().trim() == 'ausstellung') {
+        alert('10. Weltausstellung')
+    } else {
+        alert(':(')
+    }
+}
+
+function getPos(event, id) {
+    let x = event.clientX
+    let y = event.clientY
+    
+    var element = document.getElementById(id);
+    offset = window.scrollY
+    y = y+offset
+
+    element.style.left = x+'px'
+    element.style.top = y+'px'
+}
+
+function make_visible(id){
+    var element = document.getElementById(id);
+    element.classList.remove('inv');
+}
+
+function make_invisible(id){
+    var element = document.getElementById(id);
+    element.classList.add('inv');
+}
